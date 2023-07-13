4bf47f8d325bdc5968e34ba7095fbcf6d00d2e57
Ahsan-Ullah1871 <ahsanullahsunsbd@gmail.com>
2023-07-14 02:58:57 +0600

    - basic setup finished - redux setup done - tailwind setup done

---
+++ src/redux/api/apiSlice.ts
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+// Define a service using a base URL and expected endpoints
+export const Api = createApi({
+	reducerPath: "apis",
+	baseQuery: fetchBaseQuery({
+		baseUrl: "https://book-catalog-backend.vercel.app/api/v1/",
+	}),
+	endpoints: (builder) => ({
+		getProducts: builder.query({
+			query: () => `/product`,
+		}),
+		getProductDetails: builder.query({
+			query: (id: number | string) => `/product/${id}`,
+		}),
+	}),
+});
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useGetProductsQuery, useGetProductDetailsQuery } = Api;
+
