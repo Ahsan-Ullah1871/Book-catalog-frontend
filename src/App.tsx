23b00d656a783ffd819ec4752ecf8469b75fb173
Ahsan-Ullah1871 <ahsanullahsunsbd@gmail.com>
2023-07-14 16:56:26 +0600

    flowbite tailwind UI library setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { Button, Carousel } from "flowbite-react";
+
 const App = () => {
 	return (
 		<div className="h-screen bg-green-200 text-orange-300 text-4xl">
-			Hello world
+			<Carousel>
+				<img
+					alt="..."
+					src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
+				/>
+				<img
+					alt="..."
+					src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
+				/>
+				<img
+					alt="..."
+					src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
+				/>
+				<img
+					alt="..."
+					src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
+				/>
+				<img
+					alt="..."
+					src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
+				/>
+			</Carousel>
 		</div>
 	);
 };
