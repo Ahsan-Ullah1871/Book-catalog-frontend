0144f0f879f4607c05cb25e50ccdc3bacada1c40
Ahsan-Ullah1871 <ahsanullahsunsbd@gmail.com>
2023-07-15 13:02:55 +0600

    - reviews card created - reviews carousel created

--- src/components/ui/ReviewsCarousel.tsx
+++ src/components/ui/ReviewsCarousel.tsx
@@ -1,4 +1,4 @@
-import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Autoplay } from "swiper/modules";
 
 // Import Swiper styles
